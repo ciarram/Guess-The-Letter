781bed3bfc9f20e0dcc9f12f34a08fb8c57b04ba
Ciarra Murphy <mciarra@ymail.com>
2017-08-23 18:13:59 -0400

    changed for loop and added another if statement

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,24 @@
-var computerGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var playerGuess 
+var computerGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
 var win = 0;
 var loss = 0;
 var guessLeft = 9;
 
 var compLetter = computerGuess[Math.floor(Math.random() * computerGuess.length)];
 
-for(var choice= 0; choice<guessLeft; choice++){
+document.onkeyup= function(event){
+    var playerGuess = event.key;
+
+for(var choice= 0; choice < compLetter.length; choice++){
     if(playerGuess == compLetter){
         console.log("player wins!");
-        //win++;
+        win++;
     }else if(playerGuess !== compLetter){
         console.log("you lose a guess");
-        //guessLeft--;
+        guessLeft--;
+        //var oneLessGuess = guessLeft;
     }
+}
+if(guessLeft == 0 ){
+    console.log("start over");
+}
 }
