98d4192f83f18b20789bded332d32ffb178a4149
Ciarra Murphy <mciarra@ymail.com>
2017-08-23 17:42:34 -0400

    added reset css and for loop to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,17 @@
 var computerGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var playerGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var playerGuess 
 var win = 0;
 var loss = 0;
 var guessLeft = 9;
 
+var compLetter = computerGuess[Math.floor(Math.random() * computerGuess.length)];
+
+for(var choice= 0; choice<guessLeft; choice++){
+    if(playerGuess == compLetter){
+        console.log("player wins!");
+        //win++;
+    }else if(playerGuess !== compLetter){
+        console.log("you lose a guess");
+        //guessLeft--;
+    }
+}
