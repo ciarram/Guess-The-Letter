eeb8730fc92844e1cbf8ec59aef61213f4fd81f5
Ciarra Murphy <mciarra@ymail.com>
2017-08-23 17:06:46 -0400

    added variables to js file and ids to html

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+var computerGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var playerGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var win = 0;
+var loss = 0;
+var guessLeft = 9;
+
